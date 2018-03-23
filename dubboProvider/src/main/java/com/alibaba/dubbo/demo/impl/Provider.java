02f4e91ef9b1532b92a010e4add342a540f74dad
json <haitao.shen@bitubit.com>
2018-03-23 18:51:41 +0800

    dubbo

---
+++ dubboProvider/src/main/java/com/alibaba/dubbo/demo/impl/Provider.java
@@ -0,0 +1,15 @@
+package com.alibaba.dubbo.demo.impl;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Provider {
+    public static void main(String[] args) throws IOException{
+        ClassPathXmlApplicationContext c = new ClassPathXmlApplicationContext("../resources/provider.xml");
+        System.out.println(c.getDisplayName() + ":here");
+        c.start();
+        System.out.println("服务已启动");
+        System.in.read();
+    }
+}
